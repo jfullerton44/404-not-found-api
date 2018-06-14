33c7de9433c0b90da4d71f94d5f312e822d943d0
Jacob Fullerton <fullertonj44@gmail.com>
2018-06-14 17:00:48 +0200

    readd donation post

--- src/controllers/donation.controller.ts
+++ src/controllers/donation.controller.ts
@@ -15,39 +15,40 @@ export class DonationController {
   ) { }
 
 
-
-  /*@post('/donations/{user_id}/{charity_id}')
-  async createDonation(
-    @param.path.number('user_id') user_id: number,
-    @param.path.number('charity_id') charity_id: number,
-    @param.query.number('donation_amount') donation_amount: number,
-    @param.query.date('date') date: Date
-  ) {
-    var donation = new Donation;
-
-    let userExists: boolean = !!(await this.userRepo.count({ id: user_id }));
-
-    if (!userExists) {
-      throw new HttpErrors.Unauthorized('User Does not exist');
+  /*
+    @post('/donations2/{user_id}/{charity_id}')
+    async createDonation(
+      @param.path.number('user_id') user_id: number,
+      @param.path.number('charity_id') charity_id: number,
+      @param.query.number('donation_amount') donation_amount: number,
+      @param.query.date('date') date: Date
+    ) {
+      var donation = new Donation;
+  
+      let userExists: boolean = !!(await this.userRepo.count({ id: user_id }));
+  
+      if (!userExists) {
+        throw new HttpErrors.Unauthorized('User Does not exist');
+      }
+      // if (userExists) {
+      //   console.log('user exists')
+      // }
+  
+      donation.user_id = user_id;
+  
+      let charityExists: boolean = !!(await this.charityRepo.count({ id: charity_id }));
+  
+      if (!charityExists) {
+        throw new HttpErrors.Unauthorized('Charity Does not exist');
+      }
+  
+      donation.charity_id = charity_id;
+      console.log(donation_amount);
+      donation.amount_donated = donation_amount;
+  
+      return await this.donationRepo.create(donation);
     }
-    if (userExists) {
-      console.log('user exists')
-    }
-
-    donation.user_id = user_id;
-
-    let charityExists: boolean = !!(await this.charityRepo.count({ id: charity_id }));
-
-    if (!charityExists) {
-      throw new HttpErrors.Unauthorized('Charity Does not exist');
-    }
-
-    donation.charity_id = charity_id;
-    donation.amount_donated = donation_amount;
-
-    return await this.donationRepo.create(donation);
-  }
-
+  */
   @post('/donations')
   async newDonation(
     @requestBody() donation: Donation
@@ -64,7 +65,7 @@ export class DonationController {
       throw new HttpErrors.Unauthorized('Charity Does not exist');
     }
     return await this.donationRepo.create(donation);
-  } */
+  }
 
   @get('/donations')
   async getAllDonations(): Promise<Array<Donation>> {
